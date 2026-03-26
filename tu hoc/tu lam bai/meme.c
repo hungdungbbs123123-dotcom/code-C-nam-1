5a58466cb60bffc1c851dfbec3486035496a1dae
Pham Hung Dung <hungdungbbs123123@gmail.com>
2026-03-26 19:37:34 +0700

    thaydoi

---
+++ tu hoc/tu lam bai/meme.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a, c;
+    char b;  
+
+    printf("nhap so thu nhat: ");
+    scanf("%d", &a);
+    printf("nhap dau + - * / : ");
+    scanf(" %c", &b); 
+    printf("nhap so thu hai: ");
+    scanf("%d", &c);
+
+    printf("hello world\n");  
+
+    return 0;
+}
